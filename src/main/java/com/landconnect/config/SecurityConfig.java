db9aa6dcb9d5dad07b58b07958e5025f4eafbb4f
govindkumar35322 <govindkumar35322@gmail.com>
2026-07-11 00:14:31 +0530

    feat: implement JWT authentication with Spring Security

--- src/main/java/com/landconnect/config/SecurityConfig.java
+++ src/main/java/com/landconnect/config/SecurityConfig.java
@@ -1,22 +1,26 @@
 package com.landconnect.config;
 
 import com.landconnect.security.CustomUserDetailsService;
+import com.landconnect.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
    private final CustomUserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -24,11 +28,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf->csrf.disable())
-                .authenticationProvider(authenticationProvider())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
    }
    @Bean
--- src/main/java/com/landconnect/security/jwt/JwtService.java
+++ src/main/java/com/landconnect/security/jwt/JwtService.java
@@ -67,7 +67,7 @@ public class JwtService {
     private boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
-    private boolean isTokenValid(String token,UserDetails  userDetails){
+    public boolean isTokenValid(String token,UserDetails  userDetails){
         final String username=extractUsername(token);
         return username.equals(userDetails.getUsername())
                 && !isTokenExpired(token);
