1eb1cba9f4daf232f023bba925ccb486cf1f7b4c
govindkumar35322 <govindkumar35322@gmail.com>
2026-07-23 21:24:10 +0530

    Fix:springdoc version,security config

--- src/main/java/com/landconnect/config/SecurityConfig.java
+++ src/main/java/com/landconnect/config/SecurityConfig.java
@@ -33,7 +33,11 @@ public class SecurityConfig {
                 .sessionManagement(session->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**","/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/webjars/**").permitAll()
                         .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
